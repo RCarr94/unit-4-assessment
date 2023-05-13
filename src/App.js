74d314de365a108369867af0026d882f012de500
Ryan Carr <ryan.g.carr@hotmail.com>
2023-05-13 15:25:17 -0400

    add component folders and provided css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
-        <main>YOUR TWO COMPONENTS WILL GO HERE</main>
-      </div>
-    );
-  }
+export default function App (){
+
+ return (
+  <div className="App">
+   <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
+   <main>
+    YOUR TWO COMPONENTS WILL GO HERE
+   </main>
+  </div>
+ );
 }
 
-export default App;
+
